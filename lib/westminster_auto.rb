f42a3b4d41c5a5ac9eb0c71d86ea5d82d9371b64
jrog20 <jaimerogstad@gmail.com>
2020-08-24 15:06:58 -0400

    Update require links

--- lib/westminster_auto.rb
+++ lib/westminster_auto.rb
@@ -1,4 +1,8 @@
-require "westminster_auto/version"
+require_relative "./westminster_auto/version"
+require_relative "./westminster_auto/cli"
+require_relative "./westminster_auto/scraper"
+require_relative "./westminster_auto/vehicle"
 
 module WestminsterAuto
 end
+
