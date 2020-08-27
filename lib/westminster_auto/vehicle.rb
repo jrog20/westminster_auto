6c049b10d2ee7c478c5c90cc40c207ab33d312bc
jrog20 <jaimerogstad@gmail.com>
2020-08-27 11:49:06 -0400

    Refine class

--- lib/westminster_auto/vehicle.rb
+++ lib/westminster_auto/vehicle.rb
@@ -1,19 +1,13 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 class WestminsterAuto::Vehicle
 
-  attr_accessor 
+  attr_accessor :body, :transmission, :drive, :fuel, :engine
   attr_reader :year, :make, :model, :mileage, :price
 
   @@all = []
-
-  def self.new # needs to use object from CLI class as argument?
-    self.new(
-      WestminsterAuto::Scraper.inventory_basics[0],
-      WestminsterAuto::Scraper.inventory_basics[1],
-      WestminsterAuto::Scraper.inventory_basics[2],
-      # need to get mileage code,
-      WestminsterAuto::Scraper.price
-      )
-  end
 
   def initialize(year=nil, make=nil, model=nil, mileage=nil, price=nil)
     @year = year
@@ -31,7 +25,5 @@ class WestminsterAuto::Vehicle
   def self.find(id)
     self.all[id-1]
   end
-
-
 
 end
