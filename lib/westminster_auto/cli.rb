9374648a4bc36ff2fb94005c324bcfb96e39a892
jrog20 <jaimerogstad@gmail.com>
2020-08-27 18:10:05 -0400

    Refine start

--- lib/westminster_auto/cli.rb
+++ lib/westminster_auto/cli.rb
@@ -7,42 +7,56 @@ module WestminsterAuto
 
     def call
       WestminsterAuto::Scraper.get_vehicles
-      puts "Welcome to the Westminster Auto available vehicle list"
+      puts ""
+      puts "Welcome to the Westminster Auto available vehicle list:"
       start
     end
 
     def start
       print_vehicles
+      puts ""
       puts "What number vehicle would you like to see more information on?"
       input = gets.strip.to_i
-      vehicle = WestminsterAuto::Vehicle.find(input.to_i)
-      print_vehicle_details(vehicle)
+      
+      if input > 0 && input <= WestminsterAuto::Vehicle.all.count
+        vehicle = WestminsterAuto::Vehicle.find(input)
+        print_vehicle_details(vehicle)
+      else
+        puts ""
+        puts "Please enter a valid number."
+        start
+      end
 
+      puts ""
       puts "Would you like to see another vehicle? Enter Y or N"
       input = gets.strip.downcase
 
       if input == "y"
         start
       elsif input == "n"
-        puts "Have a great day!"
+        puts ""
+        puts "Thank you for viewing our inventory. Have a great day!"
+        puts ""
         exit
       else
+        puts ""
         puts "Let me show you our current inventory."
         start
       end
     end
 
-    def print_vehicles(vehicle)
+    def print_vehicles
       WestminsterAuto::Vehicle.all.each_with_index do |vehicle, index|
-      puts "#{index}."
+      puts ""
+      puts "#{index + 1}."
       puts "#{vehicle.year} #{vehicle.make} #{vehicle.model}"
       puts "Mileage: #{vehicle.mileage}"
       puts "Price: #{vehicle.price}"
       end
     end
 
-    def print_vehicle_details(input)
-      WestminsterAuto::Vehicle.all[input.to_i-1]
+    def print_vehicle_details(vehicle)
+      puts ""
       puts "#{vehicle.year} #{vehicle.make} #{vehicle.model}"
       puts "Mileage: #{vehicle.mileage}"
       puts "Price: #{vehicle.price}"
