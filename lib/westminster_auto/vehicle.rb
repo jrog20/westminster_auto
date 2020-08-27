de8ac9e23518d345c56d19bf7e060e99d13d5966
jrog20 <jaimerogstad@gmail.com>
2020-08-27 18:15:52 -0400

    Add attr_accessors

--- lib/westminster_auto/vehicle.rb
+++ lib/westminster_auto/vehicle.rb
@@ -2,28 +2,30 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-class WestminsterAuto::Vehicle
+module WestminsterAuto
+  class Vehicle
 
-  attr_accessor :body, :transmission, :drive, :fuel, :engine
-  attr_reader :year, :make, :model, :mileage, :price
+    attr_accessor :body, :transmission, :drive, :fuel, :engine
+    attr_reader :year, :make, :model, :mileage, :price
 
-  @@all = []
+    @@all = []
 
-  def initialize(year=nil, make=nil, model=nil, mileage=nil, price=nil)
-    @year = year
-    @make = make
-    @model = model
-    @mileage = mileage
-    @price = price
-    @@all << self
+    def initialize(year=nil, make=nil, model=nil, mileage=nil, price=nil)
+      @year = year
+      @make = make
+      @model = model
+      @mileage = mileage
+      @price = price
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.find(id)
+      self.all[id-1]
+    end
+
   end
-
-  def self.all
-    @@all
-  end
-
-  def self.find(id)
-    self.all[id-1]
-  end
-
 end
