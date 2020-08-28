273405e865bb853e84c86207970cdc1a69824f8c
jrog20 <jaimerogstad@gmail.com>
2020-08-28 09:51:05 -0400

    Update vehicle initialize to include all vehicle details

--- lib/westminster_auto/vehicle.rb
+++ lib/westminster_auto/vehicle.rb
@@ -5,17 +5,21 @@ require 'open-uri'
 module WestminsterAuto
   class Vehicle
 
-    attr_accessor :body, :transmission, :drive, :fuel, :engine
-    attr_reader :year, :make, :model, :mileage, :price
+    attr_reader :year, :make, :model, :mileage, :price, :body, :transmission, :drive, :fuel, :engine
 
     @@all = []
 
-    def initialize(year=nil, make=nil, model=nil, mileage=nil, price=nil)
+    def initialize(year=nil, make=nil, model=nil, mileage=nil, price=nil, body=nil, transmission=nil, drive=nil, fuel=nil, engine=nil)
       @year = year
       @make = make
       @model = model
       @mileage = mileage
       @price = price
+      @body = body
+      @transmission = transmission
+      @drive = drive
+      @fuel = fuel
+      @engine = engine
       @@all << self
     end
 
